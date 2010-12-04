85bd70d59c5acec34d65af5581085fd4f4c0fae2
Bjorn Lindgren <nr@c64.org>
2010-12-04 15:19:12 +0100

    updates to metadata constructor

--- src/libocean/metadata.c
+++ src/libocean/metadata.c
@@ -1,5 +1,7 @@
 #include <stdio.h> /* printf */
-#include <stdlib.h> /* malloc */
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 #include <libocean.h>
 
 /* Desc: metadata chunk constructor
@@ -8,13 +10,49 @@
 char *
 os_meta_create(int fd)
 {
-	char *meta, *file_hash;
+	void *meta = NULL;
+	void *ptr;
+	char *file_hash;
+	u_int32_t generation = 7;
+	u_int32_t test_gen = 0;
+	char test_hash[41];
+	int meta_size = 0;
+
+	/* add generation number */
+	meta_size = meta_size + sizeof(u_int32_t);
+	meta = realloc(meta, meta_size);
+	ptr = meta; /* save the start address */
+
+	printf("reallocated to %d bytes\n", meta_size);
+
+	memcpy(meta, &generation, sizeof(int32_t));
+
+	memcpy(&test_gen, meta, sizeof(u_int32_t));
+
+	printf("extracted gen from meta: \"%d\"\n", test_gen);
 
 	file_hash = os_sha1_file(fd);
 
-	printf("file hash: %s\n", file_hash);
+	printf("file hash: %s (%d bytes)\n", file_hash, (int) strlen(file_hash));
 
-	meta = malloc(1000);
+	printf("addr of meta before: %p\n", meta);
+
+	meta_size = meta_size + (40 * sizeof(int8_t));
+	meta = realloc(meta, meta_size);
+	printf("reallocated to %d bytes\n", meta_size);
+
+	meta = (char *) meta + sizeof(u_int32_t);
+
+	printf("addr of meta after: %p\n", meta);
+
+
+	memcpy(meta, file_hash, 40*sizeof(int8_t));
+
+	meta = ptr;
+
+	memcpy(&test_hash, (char *) meta+sizeof(u_int32_t), 40*sizeof(int8_t));
+	test_hash[40] = '\0';
+	printf("extracted hash from meta: \"%s\"\n", test_hash);
 
 	return meta;
 }
