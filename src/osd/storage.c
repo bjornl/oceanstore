7194953b9dd0983dda0297ff8445a85b296bb182
Bjorn Lindgren <nr@c64.org>
2010-12-06 19:40:04 +0100

    updates to osd storage functionality

--- src/osd/storage.c
+++ src/osd/storage.c
@@ -1,7 +1,25 @@
 #include <stdio.h> /* printf */
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <libocean.h>
 
 void
 os_store(void *chunk, int size)
 {
-	printf("storing data\n");
+	char *hash;
+	int fd, len;
+
+	hash = os_sha1(chunk, size);
+
+	fd = open(hash, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+	len = write(fd, chunk, size);
+	close(fd);
+
+	printf("chunk %s of %d bytes written to disk\n", hash, len);
+
+	free(hash);
 }
