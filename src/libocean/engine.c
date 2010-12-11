7137dbcc8c445b048e0c94521efcdd0234624fff
Bjorn Lindgren <nr@c64.org>
2010-12-11 15:48:30 +0100

    add store functionality to workunit engine

--- src/libocean/engine.c
+++ src/libocean/engine.c
@@ -1,9 +1,12 @@
 #include <stdio.h> /* printf */
 #include <wool.h>
+#include <libocean.h>
 
-TASK_1(int, engine, int, n)
+TASK_1(int, engine, struct workunit *, wu)
 {
-	printf("Engine called with arg: %d\n", n);
+	printf("Engine called with arg: %p\n", (void *) wu);
+
+	os_store(wu->chunk, wu->size);
 
 	return 0;
 }
--- src/libocean/pipeline.c
+++ src/libocean/pipeline.c
@@ -3,14 +3,14 @@
 
 #include <libocean.h>
 
-int CALL_engine (struct _Task *, int);
+int CALL_engine (struct _Task *, struct workunit *);
 
 void
 os_pipeline_push(int id, struct workunit *wu)
 {
 	printf("pushed work unit %p on pipeline #%d\n", (void *) wu, id);
 
-	ROOT_CALL(engine, 10);
+	ROOT_CALL(engine, wu);
 }
 
 struct workunit *
