c94c30f0f7594745235cda2c4cfbca876772bd5d
Bjorn <bjorn@latitude.(none)>
2010-12-17 00:28:32 +0100

    update to engine in shared library

--- src/libocean/engine.c
+++ src/libocean/engine.c
@@ -1,4 +1,5 @@
 #include <stdio.h> /* printf */
+#include <stdlib.h>
 #include <wool.h>
 #include <libocean.h>
 
@@ -7,6 +8,9 @@ TASK_1(int, engine, struct workunit *, wu)
 	printf("Engine called with arg: %p\n", (void *) wu);
 
 	os_store(wu->chunk, wu->size);
+
+	free(wu->chunk);
+	free(wu);
 
 	return 0;
 }
